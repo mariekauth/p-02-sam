64a8b3a894870fc3b55f691899d9a6eba9e70c59
Marie Kauth <marie.kauth@gmail.com>
2023-08-27 09:46:07 -0400

    Modified to accept parameters

--- sam-app-dotnet6/src/HelloWorld/Function.cs
+++ sam-app-dotnet6/src/HelloWorld/Function.cs
@@ -6,6 +6,7 @@ using System.Text.Json;
 
 using Amazon.Lambda.Core;
 using Amazon.Lambda.APIGatewayEvents;
+using System;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -30,12 +31,16 @@ namespace HelloWorld
 
         public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyRequest apigProxyEvent, ILambdaContext context)
         {
+            var c = context.InvokedFunctionArn;
 
             var location = await GetCallingIP();
             var body = new Dictionary<string, string>
             {
                 { "message", "hello world" },
-                { "location", location }
+                { "location", location },
+                { "ItemIds", GetSelectedItemId(apigProxyEvent) },
+                { "context", context.ToString() },
+                { "arn", c }
             };
 
             return new APIGatewayProxyResponse
@@ -44,6 +49,40 @@ namespace HelloWorld
                 StatusCode = 200,
                 Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
             };
+        }
+
+        private string GetSelectedItemId(APIGatewayProxyRequest request) {
+            return GetValidItemId(request);
+        }
+
+        private string GetValidItemId(APIGatewayProxyRequest request) {
+            var s = GetQueryStringItem(request, "ItemId");
+            string[] validIds = { 
+                "03b90a57-c061-4edd-99ed-21133ae4c04f", 
+                "3e866916-7ed1-4eb6-8427-093eb621af93"
+                };
+            
+            if (
+                !String.IsNullOrEmpty(s)
+                && s.Length == 36
+                && validIds.Contains(s)
+            )
+            {
+                return s;
+            } else {
+                return string.Empty;
+            }
+        }
+
+        private string GetQueryStringItem(APIGatewayProxyRequest request, string key) {
+            var q = GetQueryString(request);
+            return q.ContainsKey(key) 
+                ? q[key] 
+                : string.Empty;
+        }
+
+        private IDictionary<string, string> GetQueryString(APIGatewayProxyRequest request) {
+            return request.QueryStringParameters ?? new Dictionary<string, string>();
         }
     }
 }
